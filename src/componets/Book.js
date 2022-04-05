f9571890ff6be07f88be13252659eb060704e14a
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-05 22:11:21 +0430

    Fix style

--- src/componets/Book.js
+++ src/componets/Book.js
@@ -1,19 +1,10 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const Book = ({ category, title, author }) => (
   <li>
-    <h3>
-      $
-      {category}
-    </h3>
-    <h2>
-      $
-      {title}
-    </h2>
-    <h3>
-      $
-      {author}
-    </h3>
+    <h3>{category}</h3>
+    <h2>{title}</h2>
+    <h3>{author}</h3>
     <button type="button">Remove</button>
   </li>
 );
