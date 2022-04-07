0dbf349338262fb5252dcb51066060fe4c4ccc75
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-07 13:54:48 +0430

    Add database file

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { categoriesStatus } from '../../redux/categories/categories';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { categoriesStatus } from "../../redux/categories/categories";
 
 const Categories = () => {
   const Categories = useSelector((state) => state.categoriesReducer);
   const dispatch = useDispatch();
-  const handleAction = () => (
-    dispatch(categoriesStatus())
-  );
+  const handleAction = () => dispatch(categoriesStatus());
 
   return (
     <>
-      <button type="button" onClick={handleAction}>Check Status</button>
+      <button type="button" onClick={handleAction}>
+        Check Status
+      </button>
       {Categories}
     </>
   );
