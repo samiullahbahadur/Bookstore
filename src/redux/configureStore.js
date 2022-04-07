e5922608ab0792d2cce7af36493c34935713c420
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-07 14:00:37 +0430

    Change the cofigurationstore file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,13 @@
-import { combineReducers, createStore } from 'redux';
-import categoriesReducer from './categories/categories';
-import reducer from './books/books';
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import categoriesReducer from "./categories/categories";
+import reducer from "./books/books";
 
-const root = combineReducers(
-  {
-    categoriesReducer,
-    reducer,
-  },
-);
+const root = combineReducers({
+  categoriesReducer,
+  reducer,
+});
 
-const store = createStore(root);
+const store = createStore(root, applyMiddleware(thunk));
+store.dispatch(reducer);
 export default store;
