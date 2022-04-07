2ee8a4cac51912ece1d5fae3d918caabbc6cffbc
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-07 14:19:44 +0430

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import { Provider } from 'react-redux';
-import App from './App';
-import Navbar from './components/Navbar/Navbar';
-import reportWebVitals from './reportWebVitals';
-import Categories from './components/Categories/Categories';
-import store from './redux/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import Navbar from "./components/Navbar/Navbar";
+
+import Categories from "./components/Categories/Categories";
+import store from "./redux/configureStore";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,10 +21,5 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
